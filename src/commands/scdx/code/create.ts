4bbec1d5625fa60c2cd4739862ad670abf3842bd
Dhanik Lal Sahni <dhanik.sahni@conduent.com>
2020-06-07 21:40:10 +0530

    Code Added

---
+++ src/commands/scdx/code/create.ts
@@ -0,0 +1,47 @@
+import { flags, SfdxCommand } from '@salesforce/command';
+import { Messages } from '@salesforce/core';
+import * as fse from 'fs-extra';
+import * as path from 'path';
+import {Code} from '../../../lib/code';
+
+Messages.importMessagesDirectory(__dirname);
+const messages = Messages.loadMessages('salesforcecodex-dx-plugin', 'create');
+
+export default class Create extends SfdxCommand {
+      // Comment this out if your command does not support a hub org username
+  protected static supportsDevhubUsername = false;
+
+  // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
+  protected static requiresProject = false;
+
+    public static args = [{name: 'file'}];
+
+    protected static flagsConfig = {
+      // flag with a value (-n, --name=VALUE)
+      template: flags.string({char: 't', description: messages.getMessage('templateFlagDescription'), required: false}),
+      outputdir: flags.string({char: 'd', description: messages.getMessage('outputdirFlagDescription'), required: false}),
+      vars: flags.string({char: 'v', description: messages.getMessage('varsFlagDescription'), required: false})
+    };
+
+
+    public async run()
+    {
+      const template = this.flags.template;
+      const outputdir = this.flags.outputdir;
+      const vars = this.flags.vars;
+     
+      let templateFolder = path.join(__dirname, '../../../../templates', template);
+      if (!fse.existsSync(templateFolder)) {
+        templateFolder = path.join(__dirname, '../../../templates', template);
+      }
+      let code = new Code();
+
+      code.createFiles(templateFolder, template, vars, outputdir, (err, success) => {
+        if (err) {
+          console.error('ERROR:', err);
+          process.exit(1);
+        }
+        console.log(success);
+      });
+    }
+}
---
+++ src/lib/code.ts
@@ -0,0 +1,124 @@
+import * as fs from 'fs';
+import * as fse from 'fs-extra';
+import * as path from 'path';
+
+export class Code
+{
+  updateContent=function(content, values) {
+      const splitValues = values.split('=');
+    
+      const varName = splitValues[0];
+      const varValue = splitValues[1];
+    
+      content = content.replace(new RegExp(`{{${varName}}}`, 'g'), varValue);
+      return content;
+  }
+  
+  validateVars=function name(vars,template) {
+      if(template==='trigger' && !vars.includes('objectName')) return false;
+      return true;
+  }
+
+  fileName=function name(vars) {
+    const splitVars = vars.split(',');
+    var name='';
+    splitVars.map(function(obj) {
+      const values = obj.split('=');
+      if(values[0]==='objectName' && values[0]!==null)
+      {
+        name=values[1];
+      }
+    });
+    return name;
+  }
+  isCustomObject=function name(name) {
+      if(name.indexOf('__c')>0)
+      {
+        return true;
+      }
+      return false;
+  }
+
+  createFiles=function(templateFolder, template, vars, outputdir, done) {
+      if (!outputdir) {
+        outputdir = '.';
+      }
+
+      var name=this.fileName(vars);
+      if(name=='')
+      {
+        done('objectName is required', null);
+      }
+
+      var isCustomObject=this.isCustomObject(name);
+      if(isCustomObject)
+      {
+          vars=vars.replace('objectName','entityName');
+          name=name.replace('__c','').replace('_','');
+          vars=vars+',objectName='+name;
+      }
+      if (!this.validateVars(vars,template)) {
+        done('objectName is required for trigger generation', null);
+      }
+
+      if (!fse.existsSync(templateFolder)) {
+        done(`specified template '${template}' doesn't exist`, null);
+      }
+
+      const defJsonPath = path.join(templateFolder, 'def.json');
+    
+      if (!fse.existsSync(defJsonPath)) {
+        done('def.json not found', null);
+      }
+    
+      const defJson = JSON.parse(fs.readFileSync(defJsonPath).toString());
+      const defJsonVars = defJson.vars;
+      const defJsonBundle = defJson.bundle;
+      const defJsonFiles = defJson.files;
+    
+      if (!vars) {
+        done(`The following variables are required: ${defJsonVars}. Specify them like: -v className=myclass,apiName=48.0`, null);
+      }
+    
+      const filesCreated = [];
+    
+      defJsonFiles.forEach((row) => {
+        const fileName = row[0];
+        const fileExtension = row[1];
+    
+        if (fileName !== 'def.json') {
+    
+          const templateFilePath = path.join(templateFolder, fileName);
+          let content = fs.readFileSync(templateFilePath).toString();
+    
+          if (vars.includes(',')) {
+            const splitVars = vars.split(',');
+            splitVars.forEach((value) => {
+              content = this.updateContent(content, value);
+            });
+          } else {
+            content = this.updateContent(content, vars);
+          }
+    
+          let newFile = path.join(outputdir, `${name}${fileExtension}`);
+          // if bundle flagged as true, add all the files in a folder with the same name
+          if (defJsonBundle) {
+            newFile = path.join(outputdir, name, `${name}${fileExtension}`);
+          } 
+    
+          const newFilePath = path.dirname(newFile);
+    
+          fse.ensureDirSync(newFilePath);
+          fs.writeFileSync(newFile, content);
+          filesCreated.push(newFile);
+        }
+      });
+    
+      let result = 'The following files were created:';
+      for (let i = 0; i < filesCreated.length; i++) {
+        result += `\n  ${filesCreated[i]}`;
+      }
+    
+      done(null, result);
+  }
+};
